29707f75ed2948e40cf3024a413d561e7662ef67
Timofei <timmi202116202@gmail.com>
2024-06-20 13:13:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+'''Avto_list1 = ['Suzuky', 'Lada', 'BMW', 'Kia', 'Reno']
+count = len(Avto_list1)
+
+print(count)'''
+
+
+"""first = 20
+second = 5
+
+result_sum = (first + second)
+#print(result_sum)
+result_diff = (first - second)
+#print(result_diff)
+
+print(result_sum, result_diff)"""
+
+
+'''first = 18
+second = 9
+third = 3
+result_mean = ((first + second + third)/3)
+print(result_mean)'''
+
+'''a = 6
+b = 4
+c = 2
+f = ((a * b) + (a * c))
+print (f**3/2)'''
